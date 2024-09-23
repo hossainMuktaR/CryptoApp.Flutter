e06a777fe882ddefce877caeef6befa79fdc805c
Hossain Muktar <HossainMuktarDev@gmail.com>
2024-09-23 17:13:04 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CryptoApp',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("CryptoApp"),
+        ),
+        body: Center(child: Text("Hello World")),
+      ),
+    );
+  }
+}
